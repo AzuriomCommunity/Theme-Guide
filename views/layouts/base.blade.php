05495a29ff829d761c7685af9f310d77aca22ded
MrMicky <git@mrmicky.fr>
2022-08-21 15:08:33 +0200

    Remove Google Fonts

--- views/layouts/base.blade.php
+++ views/layouts/base.blade.php
@@ -48,9 +48,7 @@
     @stack('scripts')
 
     <!-- Fonts -->
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Quicksand:wght@400;500;600&display=swap" rel="stylesheet">
+    <link href="https://fonts.bunny.net/css2?family=Quicksand:wght@400;500;600&display=swap" rel="stylesheet">
     <link href="{{ asset('vendor/bootstrap-icons/bootstrap-icons.css') }}" rel="stylesheet">
 
     <!-- Styles -->
@@ -65,9 +63,7 @@
     @yield('app')
 </div>
 
-<footer>
-    @include('elements.footer')
-</footer>
+@include('elements.footer')
 
 @stack('footer-scripts')
 
